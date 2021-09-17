c17ddac29d4fe099b3bb4966234804c8a7f6dd8f
Kyle <59974201+Dolade@users.noreply.github.com>
2021-09-16 19:48:05 -0700

    Finished the int_sort feature and implementing it into Github now

--- Array_Sort.java
+++ Array_Sort.java
@@ -4,7 +4,33 @@ import java.util.*;
 public class Array_Sort {
 
 	public static void main(String[] args) {
+		int[] nums = new int[20];
+		int[] sortNums = new int[20];
+		int upBound = 101;
+		Random rand = new Random();
+		int temp = 101;
+		int idx = 0;
 		
+		for(int i = 0; i < 20; i++) {
+			nums[i] = rand.nextInt(upBound);
+			System.out.print(nums[i] + ", ");
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < 20; i++) {
+			for(int j = 0; j < 20; j++) {
+				if(nums[j] < temp) {
+					temp = nums[j];
+					idx = j;
+				}
+			}
+			
+			sortNums[i] = temp;
+			nums[idx] = 101;
+			System.out.println(sortNums[i]);
+			temp = 101;
+		}
 		
 	}
 
